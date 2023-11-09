67bb133699c197163db02fe47e396c480f960640
bay3m <hansonsiwenbrian@gmail.com>
2023-11-09 09:01:05 +0700

    main

--- bot_manusia.py
+++ bot_manusia.py
@@ -1,4 +1,4 @@
-#jalan sih tapi gabisa respon apa apa
+#dikasih embed
 
 import discord
 from discord.ext import commands
@@ -9,20 +9,17 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-
-def run_bot():
-    client = discord.Client(intents=intents)
-    token = TOKEN_2
-
-    @bot.command()
-    async def test(ctx, arg):
-        await ctx.send(arg)
-        
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is ready')
-
-    client.run(token)
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(embed=discord.Embed(
+        title="Hello",
+        description=arg,
+        color=0xff0ff
+    ))
+    
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is ready')
 
 if __name__ == '__main__':
-    run_bot()
+    bot.run(TOKEN_2)
